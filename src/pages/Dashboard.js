3240590c82f428e649b9dce793f0baff9058fc8b
Jose1i1o <guadix88@gmail.com>
2022-01-14 10:37:12 +0100

    class component

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,16 @@
 import Dropdown from "../components/Dropdown";
 import Checkbox from "../components/Checkbox";
 import Range from "../components/Range";
+import { Container } from "react-bootstrap";
 
 
 const Dashboard = () => {
     return (
-        <div>
-          <p>Dashboard</p>
+        <Container>
           <Checkbox />
           <Dropdown />
           <Range />
-        </div>
+        </Container>
       );
 }
 
