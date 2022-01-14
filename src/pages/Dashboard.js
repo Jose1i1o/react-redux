4eb58d9493b9d3e862dbe2938564aee3d1047049
AranBeitia <arantza.beitia@gmail.com>
2022-01-14 12:59:07 +0100

    range

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,20 @@
-import Dropdown from "../components/Dropdown";
-import Checkbox from "../components/Checkbox";
-import Range from "../components/Range";
-import { Container } from "react-bootstrap";
-
+import Dropdown from '../components/Dropdown'
+import Checkbox from '../components/Checkbox'
+import MultiRange from '../components/MultiRange'
+import { Container } from 'react-bootstrap'
 
 const Dashboard = () => {
-    return (
-        <Container>
-          <Checkbox />
-          <Dropdown />
-          <Range />
-        </Container>
-      );
+  return (
+    <Container>
+      <Checkbox />
+      <Dropdown />
+      <MultiRange
+        min={0}
+        max={1000}
+        onChange={({ min, max }) => console.log(`min= ${min}, max=${max}`)}
+      />
+    </Container>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
